35497760b6dfd2aa72ab14cdcc4ae3e8dcec2311
mrenaldyr <mrenaldyr03@gmail.com>
2023-09-18 11:10:43 +0700

    chore: add twMerge into all components

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,25 +1,25 @@
 import { twMerge } from 'tailwind-merge'
 
-const Button = ({children,type,onClick = () => {}}) => {
-    let typeClass = ""
-    switch (type) {
+const Button = ({children,variant,onClick = () => {},className}) => {
+    let variantClass = ""
+    switch (variant) {
         case "primary":
-            typeClass = "text-white bg-blue-500 hover:bg-blue-600"
+            variantClass = "text-white bg-blue-500 hover:bg-blue-600"
             break;
         case "light" :
-            typeClass = "text-gray-900 bg-white border border-gray-300 hover:bg-gray-100"
+            variantClass = "text-gray-900 bg-white border border-gray-300 hover:bg-gray-100"
             break
         case "dark" : 
-            typeClass = "text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300"
+            variantClass = "text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300"
             break
         case "success" : 
-            typeClass = "text-white bg-green-500 hover:bg-green-700 focus:ring-4 focus:ring-green-300"
+            variantClass = "text-white bg-green-500 hover:bg-green-700 focus:ring-4 focus:ring-green-300"
             break
         case "danger" : 
-            typeClass = "text-white bg-red-600 hover:bg-red-800"
+            variantClass = "text-white bg-red-600 hover:bg-red-800"
             break
     }
-    return <button onClick={onClick} className={twMerge('flex items-center gap-2 font-medium rounded-md text-sm px-4 py-2.5 transition-color duration-150',typeClass)}>{children}</button>;
+    return <button onClick={onClick} className={twMerge('flex items-center gap-2 font-medium rounded-md text-sm px-4 py-2.5 transition-color duration-150',[className,variantClass])}>{children}</button>;
 }
 
 export default Button;
--- src/components/shared/Select.jsx
+++ src/components/shared/Select.jsx
@@ -1,5 +1,7 @@
-const Select = ({onChange, children}) => {
-    return <select onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-400 focus:ring-blue-400 focus:border-blue-400 block w-full p-2.5">
+import { twMerge } from "tailwind-merge";
+
+const Select = ({onChange, children, className}) => {
+    return <select onChange={onChange} className={twMerge("bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-400 focus:ring-blue-400 focus:border-blue-400 block w-full p-2.5",className)}>
         {children}
     </select>;
 }
