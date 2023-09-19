6ae79e538df25388b4f099e94d9fa158e78bbce9
Ibrahim Aji <ibrahim.aji@live.com>
2023-09-19 14:45:07 +0700

    change a tag to Link

--- src/components/Auth/components/Auth.Login.jsx
+++ src/components/Auth/components/Auth.Login.jsx
@@ -3,11 +3,12 @@
 import BaseCard from "@/components/shared/BaseCard";
 import Button from "@/components/shared/Button";
 import Input from "@/components/shared/Input";
+import Link from "next/link";
 
 export const AuthLogin = () => {
   return (
     <main className="flex h-screen flex-col items-center justify-center md:h-screen lg:py-0">
-      <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
+      <Link href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -18,8 +19,7 @@ export const AuthLogin = () => {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="lucide lucide-cat"
-          className="mr-3 "
+          className="mr-3 lucide lucide-cat"
         >
           <path d="M12 5c.67 0 1.35.09 2 .26 1.78-2 5.03-2.84 6.42-2.26 1.4.58-.42 7-.42 7 .57 1.07 1 2.24 1 3.44C21 17.9 16.97 21 12 21s-9-3-9-7.56c0-1.25.5-2.4 1-3.44 0 0-1.89-6.42-.5-7 1.39-.58 4.72.23 6.5 2.23A9.04 9.04 0 0 1 12 5Z" />
           <path d="M8 14v.5" />
@@ -27,7 +27,7 @@ export const AuthLogin = () => {
           <path d="M11.25 16.25h1.5L12 17l-.75-.75Z" />
         </svg>
         NOVATICA
-      </a>
+      </Link>
       <BaseCard className="flex justify-center">
         <div className="w-[20rem] p-6 md:space-y-6 sm:p-8 dark:bg-gray-800 rounded-lg ">
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Sign in to your account</h1>
