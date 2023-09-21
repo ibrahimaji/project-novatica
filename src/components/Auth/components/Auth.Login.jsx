7cde25f333d60cf6f58c3d0da6b3afe8ccc2a0e2
Ibrahim Aji <ibrahim.aji@live.com>
2023-09-21 09:51:41 +0700

    fix folder structure and add children props

--- src/components/Auth/components/Auth.Login.jsx
+++ src/components/Auth/components/Auth.Login.jsx
@@ -1,7 +1,47 @@
 "use client";
 
-import { AuthLayout } from "./Auth/layout/Auth.Layout";
+import { AppLogo } from "@/components/shared/AppLogo";
+import BaseCard from "@/components/shared/BaseCard";
+import Input from "@/components/shared/Input";
+import Button from "@/components/shared/Button";
+import { AuthLayout } from "../layout/Auth.Layout";
 
 export const AuthLogin = () => {
-  return <AuthLayout />;
+  return (
+    <AuthLayout>
+    <BaseCard className="flex justify-center">
+      <div className="w-[20rem] p-6 md:space-y-6 sm:p-8 dark:bg-gray-800 rounded-lg ">
+        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+          Sign in to your account
+        </h1>
+        <form className="space-y-4 md:space-y-6">
+          <label
+            for="email"
+            className="block text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your email
+          </label>
+          <Input type="text" placeholder="name@company.com" />
+          <label
+            for="password"
+            className="block text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Password
+          </label>
+          <Input type="password" placeholder="••••••••" />
+          <Button variant="primary">Sign in</Button>
+          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+            Don't have an account yet?{" "}
+            <a
+              href="#"
+              className="font-medium text-[#2563eb] hover:underline dark:text-[#3b82f6]"
+            >
+              Sign up
+            </a>
+          </p>
+        </form>
+      </div>
+    </BaseCard>
+    </AuthLayout>
+  );
 };
