e2e6f46b84bfb58d642e1072b02a35d79d7c2938
mrenaldyr <mrenaldyr03@gmail.com>
2023-09-23 08:32:20 +0700

    chore: redesign login page

--- src/components/Auth/components/Auth.Login.jsx
+++ src/components/Auth/components/Auth.Login.jsx
@@ -7,13 +7,12 @@ import Button from "@/components/shared/Button";
 
 export const AuthLogin = () => {
   return (
-
-    <BaseCard className="flex justify-center">
-      <div className="w-[20rem] p-6 md:space-y-6 sm:p-8 dark:bg-gray-800 rounded-lg ">
-        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          Sign in to your account
-        </h1>
-        <form className="space-y-4 md:space-y-6">
+    <BaseCard className="flex flex-col justify-center max-w-sm">
+      <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        Sign in to your account
+      </h1>
+      <form className="space-y-4 md:space-y-6 mt-4">
+        <div className="space-y-2">
           <label
             for="email"
             className="block text-sm font-medium text-gray-900 dark:text-white"
@@ -21,6 +20,8 @@ export const AuthLogin = () => {
             Your email
           </label>
           <Input type="text" placeholder="name@company.com" />
+        </div>
+        <div className="space-y-2">
           <label
             for="password"
             className="block text-sm font-medium text-gray-900 dark:text-white"
@@ -28,18 +29,18 @@ export const AuthLogin = () => {
             Password
           </label>
           <Input type="password" placeholder="••••••••" />
-          <Button variant="primary">Sign in</Button>
-          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-            Don't have an account yet?{" "}
-            <a
-              href="#"
-              className="font-medium text-[#2563eb] hover:underline dark:text-[#3b82f6]"
-            >
-              Sign up
-            </a>
-          </p>
-        </form>
-      </div>
+        </div>
+        <Button variant="primary">Sign in</Button>
+        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+          Don't have an account yet?{" "}
+          <a
+            href="#"
+            className="font-medium text-[#2563eb] hover:underline dark:text-[#3b82f6]"
+          >
+            Sign up
+          </a>
+        </p>
+      </form>
     </BaseCard>
   );
 };
--- src/components/Auth/layout/Auth.Layout.jsx
+++ src/components/Auth/layout/Auth.Layout.jsx
@@ -1,9 +1,8 @@
 import { AppLogo } from "@/components/shared/AppLogo";
-import { AuthLogin } from "../components/Auth.Login";
 
 export const AuthLayout = ({ children }) => {
   return (
-    <main className="flex h-screen flex-col items-center justify-center md:h-screen lg:py-0">
+    <main className="flex h-screen flex-col items-center justify-center md:h-screen lg:py-0 bg-zinc-100">
       <AppLogo />
       {children}
     </main>
